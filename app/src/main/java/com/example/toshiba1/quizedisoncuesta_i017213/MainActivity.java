31ef36558b551ed9eec7b3596587f3eea46b0772
edisoncuesta <edisoncuesta1@gmail.com>
2018-03-15 13:51:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/toshiba1/quizedisoncuesta_i017213/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.toshiba1.quizedisoncuesta_i017213;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
